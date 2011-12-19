f250517aabab1ed60f670d4839fd16bf925ecd90
Johannes Kärnstam <letstalk@johanneskarnstam.se>
2011-12-19 18:18:56 +0100

    Första Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+        <title>Basic Page Template</title>    
+		<!-- <link href="css/main.css" rel="stylesheet" type="text/css"> -->
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+    </head>
+    <body>
+		￼
+    </body>
+<html>

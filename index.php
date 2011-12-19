e2fd9226a9714a8c0842756299b71ed3924197a0
Johannes Kärnstam <letstalk@johanneskarnstam.se>
2011-12-19 18:35:23 +0100

    CSS reset och 960gs tillagt

--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 <html>
   <head>
     <meta charset="utf-8">
-        <title>Basic Page Template</title>    
-		<!-- <link href="css/main.css" rel="stylesheet" type="text/css"> -->
-		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-    </head>
-    <body>
+    <title>HTML Mall</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="css/960.css" />
+	<link rel="stylesheet" type="text/css" href="css/text.css" />
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <!-- JS -->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	
+	</head>
+	<body>
+
+	
 		￼
     </body>
 <html>
